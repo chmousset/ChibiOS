4d70b6df119fa1644e620b9400228e8211df91c7
Charles-Henri Mousset <charles-henri.mousset@railnova.eu>
2013-09-07 21:12:00 +0200

    [fix] wrong files used

--- boards/EMBEST_DMSTF4BB/board.mk
+++ boards/EMBEST_DMSTF4BB/board.mk
@@ -1,5 +1,5 @@
 # List of all the board related files.
-BOARDSRC = ${CHIBIOS}/boards/ST_STM32F4_DM_STF4BB/board.c
+BOARDSRC = ${CHIBIOS}/boards/EMBEST_DMSTF4BB/board.c
 
 # Required include directories
-BOARDINC = ${CHIBIOS}/boards/ST_STM32F4_DM_STF4BB
+BOARDINC = ${CHIBIOS}/boards/EMBEST_DMSTF4BB
